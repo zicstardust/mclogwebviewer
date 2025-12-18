37b4b396e27e60856b7219042c7a8d7ba2a88e9d
Zic <git@zicstardust.com>
2025-12-18 13:23:32 -0400

    better select log

--- src/static/script.js
+++ src/static/script.js
@@ -2,10 +2,10 @@ function hideshowDiv(id="logs_list") {
   var x = document.getElementById(id);
   if (x.style.display === "none") {
     x.style.display = "block";
-    document.getElementById("hideshowbutton").innerHTML = "Hide Logs";
+    document.getElementById("hideshowbutton").className = "btn btn-success";
   } else {
     x.style.display = "none";
-    document.getElementById("hideshowbutton").innerHTML = "Show Logs";
+    document.getElementById("hideshowbutton").className = "btn btn-outline-success";
   }
 } 
 
