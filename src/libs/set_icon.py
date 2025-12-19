495db52ccd36f0994ef9db4ff793362eaa8bdeca
Zic <git@zicstardust.com>
2025-12-19 16:32:36 -0400

    fix set_icon

--- src/libs/set_icon.py
+++ src/libs/set_icon.py
@@ -6,14 +6,15 @@ url='https://images.icon-icons.com/1381/PNG/512/minecraft_94415.png'
 
 def set_icon(static_dir,
              default_icon=url,
-             path_icon=os.environ.get("GET_ICON_PATH", '/server-icon.png')
-             ):
+             path_icon=os.environ.get("GET_ICON_PATH", '/server-icon.png')):
     if os.path.exists(f'{static_dir}/server-icon'):
         os.remove(f'{static_dir}/server-icon')
 
     if os.path.exists(path_icon):
-         if os.getenv('FLASK_DEBUG'):
-             print("Downloding default icon")
-         shutil.copyfile(path_icon, f'{static_dir}/server-icon')
+        if os.getenv('FLASK_DEBUG'):
+            print(f"Copy \"{path_icon}\"")
+        shutil.copyfile(path_icon, f'{static_dir}/server-icon')
     else:
+        if os.getenv('FLASK_DEBUG'):
+            print("Downloding default icon")
         urlretrieve(default_icon, f'{static_dir}/server-icon')
