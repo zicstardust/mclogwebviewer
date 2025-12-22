8b183e14f8f2ecc6ad6f99e7f2af238061981311
Zic <git@zicstardust.com>
2025-12-22 16:05:22 -0400

    add message "Log not found"

--- src/libs/open_log.py
+++ src/libs/open_log.py
@@ -3,4 +3,7 @@ import os
 def open_log(log_file):
     if os.getenv('FLASK_DEBUG'):
         print(f'\nOpen log: {log_file}')
-    return open(log_file, "r")
+    try:
+        return open(log_file, "r")
+    except:
+        return ["Log not found"]
